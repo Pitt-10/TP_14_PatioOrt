376ac8551fcbc3c1ee9b3e6afe4fa3f6e0fd7ed3
Pitt-10 <pedrogutman2010@gmail.com>
2026-05-27 14:33:29 -0300

    Clase 2: Mecánica de recolección funcional y modelo con materiales

---
+++ Assets/Scrips/playerCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollision : MonoBehaviour
+{
+
+    int score = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.CompareTag("Pickable"))
+        {
+            score++;
+
+            Debug.Log("Objetos recojidos: " + score);
+
+
+        Destroy(col.gameObject);
+        }
+
+    }
+}
